0db8a76ba28c7bd3a36ee989547c83d313e64fed
Tlan <2232881941@qq.com>
2023-05-09 16:21:37 +0800

    初始化1.0

---
+++ controller/src/main/java/com/smartagriculture/Controller.java
@@ -0,0 +1,12 @@
+package com.smartagriculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Controller {
+    public static void main(String[] args) {
+        SpringApplication.run(Controller.class, args);
+    }
+
+}
