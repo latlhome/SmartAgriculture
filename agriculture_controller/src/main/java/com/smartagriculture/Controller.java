94bb544c5300c98a30b3c382fad845a3955c50b6
Tlan <2232881941@qq.com>
2023-05-09 17:51:01 +0800

    初始化1.1

--- agriculture_controller/src/main/java/com/smartagriculture/Controller.java
+++ agriculture_controller/src/main/java/com/smartagriculture/Controller.java
@@ -1,9 +1,12 @@
 package com.smartagriculture;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.jhc.mapper")
 public class Controller {
     public static void main(String[] args) {
         SpringApplication.run(Controller.class, args);
