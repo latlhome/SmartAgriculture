839c69c2d3ffd05cc144e10513fd5032319f604c
Tlan <2232881941@qq.com>
2023-05-15 16:09:55 +0800

    种植日志

---
+++ agriculture_pojo/src/main/java/com/smart/agriculture/Do/PlantingLog.java
@@ -0,0 +1,31 @@
+package com.smart.agriculture.Do;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 种植日志
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlantingLog extends BaseDo{
+    /**
+     * 标题
+     */
+    @ApiModelProperty("标题")
+    private String title;
+
+    /**
+     * 内容
+     */
+    @ApiModelProperty("内容")
+    private String content;
+    /**
+     * 配图 多个图片用#间隔
+     */
+    @ApiModelProperty("配图 多个图片用#间隔")
+    private String drawing;
+}
